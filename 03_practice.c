97eb27f703c8c80858c62844c3e755f1bb0adddf
vijay pratap <vijayypratap002@gmail.com>
2022-02-01 22:06:04 +0530

    code for printing table of any no.

---
+++ 03_practice.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+    int a,i;
+    printf("Enter a NO.");
+    scanf("%d",&a);
+    for ( i = 1; i <11; i++)
+    {
+        printf("%d\n",a*i);
+    }
+    
+return 0;
+}
