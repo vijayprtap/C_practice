916bb9c65dea3678a5f5c7bc91b4c28c913ae49e
vijay pratap <vijayypratap002@gmail.com>
2022-02-26 23:13:35 +0530

    random number generator

---
+++ 05_practice.c
@@ -0,0 +1,12 @@
+//random no. generator.
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+int main(){
+    int number;
+    srand(time(0));
+    number= rand()%100+1;
+    printf("the random number is %d \n", number);
+return 0;
+}
