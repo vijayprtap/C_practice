295d4a1defca370881c2401e1a2b2777da8847c8
vijay pratap <vijayypratap002@gmail.com>
2022-03-18 16:17:15 +0530

    Ternary Operator

---
+++ 08_practice.c
@@ -0,0 +1,12 @@
+// ternary operator
+#include<stdio.h>
+
+int main(){
+int age;
+printf("Enter age");
+scanf("%d",&age);
+
+age >=18 ? printf("ADULT\n"):printf("Not ADULT\n");//TERNARY OPERTOR
+
+return 0;
+}
